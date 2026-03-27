e490bc4e21419d0ffefe7f647d5b21c36d9b58ce
Hēsperus <hesprs@outlook.com>
2026-03-27 23:20:39 +0800

    fix css module definition

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,5 +1,2 @@
 export * from 'json-canvas-viewer';
 export { default as JSONCanvasViewerComponent } from '@/Viewer.vue';
-
-// must import env.d.ts, otherwise dts bundler will throw at '*.scss?inline'
-import './env.d.ts';

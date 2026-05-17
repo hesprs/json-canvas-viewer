5df9a3af8005f2cabb1969b7b909cb4d69ac916b
Hēsperus <hesprs@outlook.com>
2026-05-18 03:01:28 +0800

    chore(deps): bunp deps (#79)

--- packages/preact/src/Viewer.tsx
+++ packages/preact/src/Viewer.tsx
@@ -81,153 +81,155 @@ function useLatest<T>(value: T) {
 	return ref;
 }
 
-export default forwardRef(function ViewerInner<T extends ModuleInputCtor>(
-	{
-		text,
-		markdown,
-		image,
-		video,
-		audio,
-		link,
-		options = {} as ViewerProps<T>['options'],
-		attachmentDir,
-		attachments,
-		canvas,
-		theme,
-		modules,
-		className,
-		style,
-		prerenderHtml,
-	}: ViewerProps<T>,
-	ref: ForwardedRef<ViewerHandle<T>>,
-) {
-	const containerRef = useRef<HTMLElement | undefined>();
-	const viewerRef = useRef<JSONCanvasViewerInterface<T> | undefined>();
-	const textRef = useLatest(text);
-	const markdownRef = useLatest(markdown);
-	const imageRef = useLatest(image);
-	const videoRef = useLatest(video);
-	const audioRef = useLatest(audio);
-	const linkRef = useLatest(link);
-	const portalsByIdRef = useRef(new Map<string, PortalEntry>());
-	const [, forceRender] = useState(0);
-
-	function upsertPortal(id: string, container: HTMLDivElement, element: ComponentChildren) {
-		portalsByIdRef.current.set(id, { container, element, id });
-		flushSync(() => forceRender((x) => x + 1));
-	}
-
-	function removePortalById(id: string) {
-		portalsByIdRef.current.delete(id);
-	}
-
-	useImperativeHandle(ref, () => ({ viewer: viewerRef.current }), []);
-
-	const nodeComponents = useMemo<Options['nodeComponents']>(() => {
-		function createNodeFunc<N extends TextSlotProps | FileSlotProps | LinkSlotProps>(
-			getRenderFn: () => ((props: N) => ComponentChildren) | undefined,
-		) {
-			return ({
-				container,
-				content,
-				node,
-				onBeforeUnmount,
-				onActive,
-				onLoseActive,
-			}: {
-				container: HTMLDivElement;
-				content: string;
-				node: N['node'];
-				onBeforeUnmount: Hook;
-				onActive: Hook;
-				onLoseActive: Hook;
-			}) => {
-				const renderFn = getRenderFn();
-				if (!renderFn) return;
-				upsertPortal(
-					node.id,
-					container,
-					renderFn({
-						content,
-						node,
-						onActive,
-						onLoseActive,
-					} as N),
-				);
-				onBeforeUnmount.subscribe(() => {
-					removePortalById(node.id);
-				});
-			};
-		}
-
-		const out: Options['nodeComponents'] = {};
-		if (text) out.text = createNodeFunc<TextSlotProps>(() => textRef.current);
-		if (markdown) out.markdown = createNodeFunc<FileSlotProps>(() => markdownRef.current);
-		if (image) out.image = createNodeFunc<FileSlotProps>(() => imageRef.current);
-		if (video) out.video = createNodeFunc<FileSlotProps>(() => videoRef.current);
-		if (audio) out.audio = createNodeFunc<FileSlotProps>(() => audioRef.current);
-		if (link) out.link = createNodeFunc<LinkSlotProps>(() => linkRef.current);
-		return out;
-		// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
-	}, [text, markdown, image, video, audio, link]);
-
-	useLayoutEffect(() => {
-		if (!containerRef.current) return;
-
-		viewerRef.current = new JSONCanvasViewer(
-			{
-				...options,
-				attachmentDir,
-				attachments,
-				canvas,
-				container: containerRef.current as unknown as HTMLDivElement,
-				nodeComponents,
-				theme,
-			} as Options<T>,
-			modules,
-		);
-
-		return () => {
-			viewerRef.current?.dispose();
-			viewerRef.current = undefined;
-			// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
-			portalsByIdRef.current.clear();
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	useEffect(() => {
-		viewerRef.current?.changeTheme(theme);
-	}, [theme]);
-
-	useEffect(() => {
-		viewerRef.current?.load({
+export default forwardRef(
+	<T extends ModuleInputCtor>(
+		{
+			text,
+			markdown,
+			image,
+			video,
+			audio,
+			link,
+			options = {} as ViewerProps<T>['options'],
 			attachmentDir,
 			attachments,
 			canvas,
-		});
-	}, [canvas, attachmentDir, attachments]);
+			theme,
+			modules,
+			className,
+			style,
+			prerenderHtml,
+		}: ViewerProps<T>,
+		ref: ForwardedRef<ViewerHandle<T>>,
+	) => {
+		const containerRef = useRef<HTMLElement | undefined>();
+		const viewerRef = useRef<JSONCanvasViewerInterface<T> | undefined>();
+		const textRef = useLatest(text);
+		const markdownRef = useLatest(markdown);
+		const imageRef = useLatest(image);
+		const videoRef = useLatest(video);
+		const audioRef = useLatest(audio);
+		const linkRef = useLatest(link);
+		const portalsByIdRef = useRef(new Map<string, PortalEntry>());
+		const [, forceRender] = useState(0);
 
-	const portals = [...portalsByIdRef.current.values()].map((p) =>
-		createPortal(p.element, p.container),
-	);
+		function upsertPortal(id: string, container: HTMLDivElement, element: ComponentChildren) {
+			portalsByIdRef.current.set(id, { container, element, id });
+			flushSync(() => forceRender((x) => x + 1));
+		}
 
-	return (
-		<>
-			<section
-				ref={(el) => {
-					if (el) containerRef.current = el;
-				}}
-				className={className}
-				style={{
-					maxHeight: '100vh',
-					maxWidth: '100vw',
-					// oxlint-disable-next-line typescript/no-misused-spread
-					...style,
-				}}
-				dangerouslySetInnerHTML={prerenderHtml ? { __html: prerenderHtml } : undefined}
-			/>
-			{portals}
-		</>
-	);
-});
+		function removePortalById(id: string) {
+			portalsByIdRef.current.delete(id);
+		}
+
+		useImperativeHandle(ref, () => ({ viewer: viewerRef.current }), []);
+
+		const nodeComponents = useMemo<Options['nodeComponents']>(() => {
+			function createNodeFunc<N extends TextSlotProps | FileSlotProps | LinkSlotProps>(
+				getRenderFn: () => ((props: N) => ComponentChildren) | undefined,
+			) {
+				return ({
+					container,
+					content,
+					node,
+					onBeforeUnmount,
+					onActive,
+					onLoseActive,
+				}: {
+					container: HTMLDivElement;
+					content: string;
+					node: N['node'];
+					onBeforeUnmount: Hook;
+					onActive: Hook;
+					onLoseActive: Hook;
+				}) => {
+					const renderFn = getRenderFn();
+					if (!renderFn) return;
+					upsertPortal(
+						node.id,
+						container,
+						renderFn({
+							content,
+							node,
+							onActive,
+							onLoseActive,
+						} as N),
+					);
+					onBeforeUnmount.subscribe(() => {
+						removePortalById(node.id);
+					});
+				};
+			}
+
+			const out: Options['nodeComponents'] = {};
+			if (text) out.text = createNodeFunc<TextSlotProps>(() => textRef.current);
+			if (markdown) out.markdown = createNodeFunc<FileSlotProps>(() => markdownRef.current);
+			if (image) out.image = createNodeFunc<FileSlotProps>(() => imageRef.current);
+			if (video) out.video = createNodeFunc<FileSlotProps>(() => videoRef.current);
+			if (audio) out.audio = createNodeFunc<FileSlotProps>(() => audioRef.current);
+			if (link) out.link = createNodeFunc<LinkSlotProps>(() => linkRef.current);
+			return out;
+			// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
+		}, [text, markdown, image, video, audio, link]);
+
+		useLayoutEffect(() => {
+			if (!containerRef.current) return;
+
+			viewerRef.current = new JSONCanvasViewer(
+				{
+					...options,
+					attachmentDir,
+					attachments,
+					canvas,
+					container: containerRef.current as unknown as HTMLDivElement,
+					nodeComponents,
+					theme,
+				} as Options<T>,
+				modules,
+			);
+
+			return () => {
+				viewerRef.current?.dispose();
+				viewerRef.current = undefined;
+				// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
+				portalsByIdRef.current.clear();
+			};
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, []);
+
+		useEffect(() => {
+			viewerRef.current?.changeTheme(theme);
+		}, [theme]);
+
+		useEffect(() => {
+			viewerRef.current?.load({
+				attachmentDir,
+				attachments,
+				canvas,
+			});
+		}, [canvas, attachmentDir, attachments]);
+
+		const portals = [...portalsByIdRef.current.values()].map((p) =>
+			createPortal(p.element, p.container),
+		);
+
+		return (
+			<>
+				<section
+					ref={(el) => {
+						if (el) containerRef.current = el;
+					}}
+					className={className}
+					style={{
+						maxHeight: '100vh',
+						maxWidth: '100vw',
+						// oxlint-disable-next-line typescript/no-misused-spread
+						...style,
+					}}
+					dangerouslySetInnerHTML={prerenderHtml ? { __html: prerenderHtml } : undefined}
+				/>
+				{portals}
+			</>
+		);
+	},
+);
--- packages/react/src/Viewer.tsx
+++ packages/react/src/Viewer.tsx
@@ -79,152 +79,154 @@ function useLatest<T>(value: T) {
 	return ref;
 }
 
-export default forwardRef(function ViewerInner<T extends ModuleInputCtor>(
-	{
-		attachmentDir,
-		attachments,
-		canvas,
-		theme,
-		modules,
-		className,
-		style,
-		prerenderHtml,
-		options = {} as ViewerProps<T>['options'],
-		text,
-		markdown,
-		image,
-		video,
-		audio,
-		link,
-	}: ViewerProps<T>,
-	ref: React.ForwardedRef<ViewerHandle<T>>,
-) {
-	const containerRef = useRef<HTMLElement | undefined>(undefined);
-	const viewerRef = useRef<JSONCanvasViewerInterface<T> | undefined>(undefined);
-	const textRef = useLatest(text);
-	const markdownRef = useLatest(markdown);
-	const imageRef = useLatest(image);
-	const videoRef = useLatest(video);
-	const audioRef = useLatest(audio);
-	const linkRef = useLatest(link);
-	const portalsByIdRef = useRef(new Map<string, PortalEntry>());
-	const [, forceRender] = useState(0);
-
-	function upsertPortal(id: string, container: HTMLDivElement, element: ReactNode) {
-		portalsByIdRef.current.set(id, { container, element, id });
-		flushSync(() => forceRender((x) => x + 1));
-	}
-	function removePortalById(id: string) {
-		portalsByIdRef.current.delete(id);
-	}
-
-	useImperativeHandle(ref, () => ({ viewer: viewerRef.current }), []);
-
-	const nodeComponents = useMemo<Options['nodeComponents']>(() => {
-		function createNodeFunc<N extends TextSlotProps | FileSlotProps | LinkSlotProps>(
-			getRenderFn: () => ((props: N) => ReactNode) | undefined,
-		) {
-			return ({
-				container,
-				content,
-				node,
-				onBeforeUnmount,
-				onActive,
-				onLoseActive,
-			}: {
-				container: HTMLDivElement;
-				content: string;
-				node: N['node'];
-				onBeforeUnmount: Hook;
-				onActive: Hook;
-				onLoseActive: Hook;
-			}) => {
-				const renderFn = getRenderFn();
-				if (!renderFn) return;
-				upsertPortal(
-					node.id,
-					container,
-					renderFn({
-						content,
-						node,
-						onActive,
-						onLoseActive,
-					} as N),
-				);
-				onBeforeUnmount.subscribe(() => {
-					removePortalById(node.id);
-				});
-			};
-		}
-
-		const out: Options['nodeComponents'] = {};
-		if (text) out.text = createNodeFunc<TextSlotProps>(() => textRef.current);
-		if (markdown) out.markdown = createNodeFunc<FileSlotProps>(() => markdownRef.current);
-		if (image) out.image = createNodeFunc<FileSlotProps>(() => imageRef.current);
-		if (video) out.video = createNodeFunc<FileSlotProps>(() => videoRef.current);
-		if (audio) out.audio = createNodeFunc<FileSlotProps>(() => audioRef.current);
-		if (link) out.link = createNodeFunc<LinkSlotProps>(() => linkRef.current);
-		return out;
-		// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
-	}, [text, markdown, image, video, audio, link]);
-
-	useLayoutEffect(() => {
-		if (!containerRef.current) return;
-
-		viewerRef.current = new JSONCanvasViewer(
-			{
-				...options,
-				attachmentDir,
-				attachments,
-				canvas,
-				container: containerRef.current as unknown as HTMLDivElement,
-				nodeComponents,
-				theme,
-			} as Options<T>,
-			modules,
-		);
-
-		return () => {
-			viewerRef.current?.dispose();
-			viewerRef.current = undefined;
-			// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
-			portalsByIdRef.current.clear();
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	useEffect(() => {
-		viewerRef.current?.changeTheme(theme);
-	}, [theme]);
-
-	useEffect(() => {
-		viewerRef.current?.load({
+export default forwardRef(
+	<T extends ModuleInputCtor>(
+		{
 			attachmentDir,
 			attachments,
 			canvas,
-		});
-	}, [canvas, attachmentDir, attachments]);
+			theme,
+			modules,
+			className,
+			style,
+			prerenderHtml,
+			options = {} as ViewerProps<T>['options'],
+			text,
+			markdown,
+			image,
+			video,
+			audio,
+			link,
+		}: ViewerProps<T>,
+		ref: React.ForwardedRef<ViewerHandle<T>>,
+	) => {
+		const containerRef = useRef<HTMLElement | undefined>(undefined);
+		const viewerRef = useRef<JSONCanvasViewerInterface<T> | undefined>(undefined);
+		const textRef = useLatest(text);
+		const markdownRef = useLatest(markdown);
+		const imageRef = useLatest(image);
+		const videoRef = useLatest(video);
+		const audioRef = useLatest(audio);
+		const linkRef = useLatest(link);
+		const portalsByIdRef = useRef(new Map<string, PortalEntry>());
+		const [, forceRender] = useState(0);
 
-	const portals = [...portalsByIdRef.current.values()].map((p) =>
-		createPortal(p.element, p.container, p.id),
-	);
+		function upsertPortal(id: string, container: HTMLDivElement, element: ReactNode) {
+			portalsByIdRef.current.set(id, { container, element, id });
+			flushSync(() => forceRender((x) => x + 1));
+		}
+		function removePortalById(id: string) {
+			portalsByIdRef.current.delete(id);
+		}
 
-	return (
-		<>
-			<section
-				ref={(el) => {
-					if (el) containerRef.current = el;
-				}}
-				className={className}
-				style={{
-					maxHeight: '100vh',
-					maxWidth: '100vw',
-					...style,
-				}}
-				dangerouslySetInnerHTML={prerenderHtml ? { __html: prerenderHtml } : undefined}
-				suppressHydrationWarning={Boolean(prerenderHtml)}
-			/>
-			{portals}
-		</>
-	);
-});
+		useImperativeHandle(ref, () => ({ viewer: viewerRef.current }), []);
+
+		const nodeComponents = useMemo<Options['nodeComponents']>(() => {
+			function createNodeFunc<N extends TextSlotProps | FileSlotProps | LinkSlotProps>(
+				getRenderFn: () => ((props: N) => ReactNode) | undefined,
+			) {
+				return ({
+					container,
+					content,
+					node,
+					onBeforeUnmount,
+					onActive,
+					onLoseActive,
+				}: {
+					container: HTMLDivElement;
+					content: string;
+					node: N['node'];
+					onBeforeUnmount: Hook;
+					onActive: Hook;
+					onLoseActive: Hook;
+				}) => {
+					const renderFn = getRenderFn();
+					if (!renderFn) return;
+					upsertPortal(
+						node.id,
+						container,
+						renderFn({
+							content,
+							node,
+							onActive,
+							onLoseActive,
+						} as N),
+					);
+					onBeforeUnmount.subscribe(() => {
+						removePortalById(node.id);
+					});
+				};
+			}
+
+			const out: Options['nodeComponents'] = {};
+			if (text) out.text = createNodeFunc<TextSlotProps>(() => textRef.current);
+			if (markdown) out.markdown = createNodeFunc<FileSlotProps>(() => markdownRef.current);
+			if (image) out.image = createNodeFunc<FileSlotProps>(() => imageRef.current);
+			if (video) out.video = createNodeFunc<FileSlotProps>(() => videoRef.current);
+			if (audio) out.audio = createNodeFunc<FileSlotProps>(() => audioRef.current);
+			if (link) out.link = createNodeFunc<LinkSlotProps>(() => linkRef.current);
+			return out;
+			// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
+		}, [text, markdown, image, video, audio, link]);
+
+		useLayoutEffect(() => {
+			if (!containerRef.current) return;
+
+			viewerRef.current = new JSONCanvasViewer(
+				{
+					...options,
+					attachmentDir,
+					attachments,
+					canvas,
+					container: containerRef.current as unknown as HTMLDivElement,
+					nodeComponents,
+					theme,
+				} as Options<T>,
+				modules,
+			);
+
+			return () => {
+				viewerRef.current?.dispose();
+				viewerRef.current = undefined;
+				// oxlint-disable-next-line eslint-plugin-react-hooks/exhaustive-deps
+				portalsByIdRef.current.clear();
+			};
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, []);
+
+		useEffect(() => {
+			viewerRef.current?.changeTheme(theme);
+		}, [theme]);
+
+		useEffect(() => {
+			viewerRef.current?.load({
+				attachmentDir,
+				attachments,
+				canvas,
+			});
+		}, [canvas, attachmentDir, attachments]);
+
+		const portals = [...portalsByIdRef.current.values()].map((p) =>
+			createPortal(p.element, p.container, p.id),
+		);
+
+		return (
+			<>
+				<section
+					ref={(el) => {
+						if (el) containerRef.current = el;
+					}}
+					className={className}
+					style={{
+						maxHeight: '100vh',
+						maxWidth: '100vw',
+						...style,
+					}}
+					dangerouslySetInnerHTML={prerenderHtml ? { __html: prerenderHtml } : undefined}
+					suppressHydrationWarning={Boolean(prerenderHtml)}
+				/>
+				{portals}
+			</>
+		);
+	},
+);
